2097fcb997fd60034dd6c6feeeb9c1c981a1237b
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-10 23:07:01 +0000

    adds ruby version to gemfile

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20171021182300) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "products", force: :cascade do |t|
     t.string "name"
     t.string "price"
