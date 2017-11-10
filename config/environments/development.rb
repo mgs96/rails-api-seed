c3cfed2932df524fee42b33c6fa217443ad7666f
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-10 23:46:51 +0000

    emails are sending in development, confirmation url still an issue

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,10 +37,19 @@ Rails.application.configure do
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
   
-  config.action_mailer.default_url_options = { host: "back-end-salazarthecoder.c9users.io" }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: "back-end-salazarthecoder.c9users.io", port: 8082 }
   config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { host: 'https://back-end-salazarthecoder.c9users.io' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    user_name:      'mauricio.guzman.salazar@gmail.com',
+    password:       '96051006469',
+    domain:         'localhost:3000',
+    address:        'smtp.gmail.com',
+    port:           '587',
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
 
   # Raises error for missing translations
