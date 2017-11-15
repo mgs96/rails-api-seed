7fa8edd7614b5029dad763631b7ed6e68b964cb9
Mauricio <mauricio.guzman.salazar@gmail.com>
2017-11-15 14:36:39 +0000

    response now should send token

--- config/application.rb
+++ config/application.rb
@@ -28,10 +28,13 @@ module Ecommerce
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     
-    config.middleware.insert_before 0, Rack::Cors do
+    config.middleware.use Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*',
+          :headers => :any,
+          :expose  => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+          :methods => [:get, :post, :options, :delete, :put]
       end
     end
     
