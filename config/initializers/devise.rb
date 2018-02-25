1385c4fdbc6b66893a383a8de1d6804cd6d955ec
Mauricio <mauricio.guzman.salazar@gmail.com>
2018-02-24 20:15:28 -0500

    changes navigational format

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,5 @@
 Devise.setup do |config|
-    config.navigational_formats = [ :json ]
+    config.navigational_formats = []
     config.mailer_sender = "mauricio.guzman.salazar@gmail.com"
     config.omniauth :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {}
 end
