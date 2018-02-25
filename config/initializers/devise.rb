5d8a0ac4336992733f251985b3438cd4d5ec2ac5
Mauricio <mauricio.guzman.salazar@gmail.com>
2018-02-24 20:30:55 -0500

    monkey patch exclamation mark

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -3,3 +3,12 @@ Devise.setup do |config|
     config.mailer_sender = "mauricio.guzman.salazar@gmail.com"
     config.omniauth :google_oauth2, ENV["GOOGLE_KEY"], ENV["GOOGLE_SECRET"], {}
 end
+
+Rails.application.config.to_prepare do              # to_prepare ensures that the monkey patching happens before the first request
+    Devise::OmniauthCallbacksController.class_eval do # reopen the class
+        def failure                                     # redefine the failure method
+        set_flash_message! :alert, :failure, kind: OmniAuth::Utils.camelize(failed_strategy.name), reason: failure_message
+        redirect_to after_omniauth_failure_path_for(resource_name)
+        end
+    end
+end
