fda4da269ad87c9b96a68df34dc578c113bb023e
Mauricio <mauricio.guzman.salazar@gmail.com>
2018-02-25 09:45:32 -0500

    another monkey patch

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,22 +12,16 @@ Rails.application.config.to_prepare do              # to_prepare ensures that th
         end
     end
 
-    class OmniAuth::Strategies::OAuth2
-        # most strategies (Facebook, GoogleOauth2) do not override this method, it means that
-        # for such strategies JSON posting of access_token will work out of the box
-        def callback_phase_with_json
-            # Doing the same thing as Rails controllers do, giving uniform access to GET, POST and JSON params
-            # reqest.params contains only GET and POST params as a hash
-            # env[..] contains JSON, XML, YAML params as a hash
-            # see ActionDispatch::Http::Parameters#parameters
-            parsed_params = env['action_dispatch.request.request_parameters']
-            if parsed_params
-                request.params['code'] = parsed_params['code'] if parsed_params['code']
-                request.params['access_token'] = parsed_params['access_token'] if parsed_params['access_token']
-                request.params['id_token'] = parsed_params['id_token'] if parsed_params['id_token'] # used by Google
+    module OmniAuth
+        module Strategies
+            class GoogleOauth2
+                def get_access_token(request)
+                json = JSON.parse(request.body.read)
+                json = json.dup.deep_transform_keys { |key| key.to_s.underscore }
+                raise "invalid token '#{json['access_token']}'" unless verify_token(json['access_token'])
+                ::OAuth2::AccessToken.from_hash(client, json)
+                end
             end
-            callback_phase_without_json
         end
-        alias_method_chain :callback_phase, :json
     end
 end
