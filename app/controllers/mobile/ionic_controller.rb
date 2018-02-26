1246c67ddb79abd5c2e7591748c1ebc81536134b
Mauricio Guzmán <mauricio.guzman.salazar@gmail.com>
2018-02-26 12:45:57 -0500

    debugging

--- app/controllers/mobile/ionic_controller.rb
+++ app/controllers/mobile/ionic_controller.rb
@@ -10,6 +10,12 @@ class Mobile::IonicController < ApplicationController
     uri = URI.parse('https://www.googleapis.com/oauth2/v3/tokeninfo')
     uri.query = URI.encode_www_form(params)
     response = Net::HTTP.get_response(uri)
-    puts response.inspect
+
+    if response.code == 200
+      data = JSON.parse(response.body)
+      puts data
+    end
+
+    render :json => data
   end
 end
